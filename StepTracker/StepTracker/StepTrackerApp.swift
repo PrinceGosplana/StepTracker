235a5251ccbab1a3033602c4bf6884c5bf8e318b
Oleksandr Isaiev <nevkluchat@gmail.com>
2024-06-15 11:22:03 +0300

    init the project

---
+++ StepTracker/StepTracker/StepTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  StepTrackerApp.swift
+//  StepTracker
+//
+//  Created by Oleksandr Isaiev on 15.06.2024.
+//
+
+import SwiftUI
+
+@main
+struct StepTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
