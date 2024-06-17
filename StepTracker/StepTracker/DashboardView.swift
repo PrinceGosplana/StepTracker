9da16f69a5e26bb6d969270727d86dc641b66d08
Oleksandr Isaiev <nevkluchat@gmail.com>
2024-06-17 07:20:44 +0300

    initial bar chart ui for steps

--- StepTracker/StepTracker/DashboardView.swift
+++ StepTracker/StepTracker/DashboardView.swift
@@ -5,6 +5,7 @@
 //  Created by Oleksandr Isaiev on 15.06.2024.
 //
 
+import Charts
 import SwiftUI
 
 struct DashboardView: View {
@@ -18,14 +19,14 @@ struct DashboardView: View {
         NavigationStack {
             ScrollView {
                 VStack(spacing: 20) {
-                    
+
                     Picker("Selected Stat", selection: $selectedStat) {
                         ForEach(HealthMetricContext.allCases) {
                             Text($0.title)
                         }
                     }
                     .pickerStyle(.segmented)
-                    
+
                     VStack {
                         NavigationLink(value: selectedStat) {
                             HStack {
@@ -33,33 +34,39 @@ struct DashboardView: View {
                                     Label("Steps", systemImage: "figure.walk")
                                         .font(.title3.bold())
                                         .foregroundStyle(.pink)
-                                    
+
                                     Text("Avg: 18K Steps")
                                         .font(.caption)
                                         .foregroundStyle(.secondary)
                                 }
-                                
+
                                 Spacer()
-                                
+
                                 Image(systemName: "chevron.right")
                                     .foregroundStyle(.secondary)
                             }
                         }
                         .padding(.bottom, 12)
-                        
-                        RoundedRectangle(cornerRadius: 12)
-                            .foregroundStyle(.secondary)
-                            .frame(height: 150)
+
+                        Chart {
+                            ForEach(hkManager.stepData) { steps in
+                                BarMark(
+                                    x: .value("Date", steps.date, unit: .day),
+                                    y: .value("Steps", steps.value)
+                                )
+                            }
+                        }
+                        .frame(height: 150)
                     }
                     .padding()
                     .background(RoundedRectangle(cornerRadius: 12).fill(Color(.secondarySystemBackground)))
-                    
+
                     VStack(alignment: .leading) {
                         VStack(alignment: .leading) {
                             Label("Averages", systemImage: "calendar")
                                 .font(.title3.bold())
                                 .foregroundStyle(.pink)
-                            
+
                             Text("Last 28 Days")
                                 .font(.caption)
                                 .foregroundStyle(.secondary)
@@ -79,28 +86,25 @@ struct DashboardView: View {
                 isShowingPermissionPrimingSheet = !hasSeenPermissionPriming
             }
             .task {
-                if hasSeenPermissionPriming {
-                    await hkManager.fetchStepCount()
-                    await hkManager.fetchWeights()
-                }
+                await hkManager.fetchStepCount()
             }
             .navigationTitle("Dashboard")
             .navigationDestination(for: HealthMetricContext.self) { metric in
                 HealthDataListView(metric: metric)
             }
             .sheet(isPresented: $isShowingPermissionPrimingSheet) {
-                
+
             } content: {
                 HealthKitPermissionPrimingView(hasSeen: $hasSeenPermissionPriming)
             }
-            
+
         }
         .tint(isSteps ? .pink : .indigo)
     }
 }
 
-#Preview {
-    DashboardView()
-        .environment(HealthKitManager())
-}
+//#Preview {
+//    DashboardView()
+//        .environment(HealthKitManager())
+//}
  
--- StepTracker/StepTracker/HealthKitManager.swift
+++ StepTracker/StepTracker/HealthKitManager.swift
@@ -14,6 +14,9 @@ import Observation
 
     let types: Set = [HKQuantityType(.stepCount), HKQuantityType(.bodyMass)]
 
+    var stepData: [HealthMetric] = []
+    var weightData: [HealthMetric] = []
+
     func fetchStepCount() async {
         let calendar = Calendar.current
         let today = calendar.startOfDay(for: .now)
@@ -25,6 +28,9 @@ import Observation
         let stepQuery = HKStatisticsCollectionQueryDescriptor(predicate: samplePredicate, options: .cumulativeSum, anchorDate: endDate, intervalComponents: .init(day: 1))
 
         let stepCounts = try! await stepQuery.result(for: store)
+        stepData = stepCounts.statistics().map {
+            .init(date: $0.startDate, value: $0.sumQuantity()?.doubleValue(for: .count()) ?? 0)
+        }
     }
 
     func fetchWeights() async {
@@ -43,6 +49,9 @@ import Observation
         )
 
         let weights = try! await weightQuery.result(for: store)
+        weightData = weights.statistics().map {
+            .init(date: $0.startDate, value: $0.mostRecentQuantity()?.doubleValue(for: .gram()) ?? 0)
+        }
     }
 
     func addSimulatorData() async {
