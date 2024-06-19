3a1a2b570c0532825ad0dc487e60cb1ee16d5ebf
Oleksandr Isaiev <nevkluchat@gmail.com>
2024-06-19 10:53:05 +0300

    code refactoring

--- StepTracker/StepTracker/DashboardView.swift
+++ StepTracker/StepTracker/DashboardView.swift
@@ -13,21 +13,8 @@ struct DashboardView: View {
     @AppStorage("hasSeenPermissionPriming") private var hasSeenPermissionPriming = false
     @State private var isShowingPermissionPrimingSheet = false
     @State private var selectedStat: HealthMetricContext = .steps
-    @State private var rawSelectedDate: Date?
 
     var isSteps: Bool { selectedStat == .steps}
-    var avgStepCount: Double {
-        guard hkManager.stepData.isEmpty else { return 0 }
-        let totalSteps = hkManager.stepData.reduce(0) { $0 + $1.value }
-        return totalSteps/Double(hkManager.stepData.count)
-    }
-
-    var selectedHealthMetric: HealthMetric? {
-        guard let rawSelectedDate else { return nil }
-        return hkManager.stepData.first {
-            Calendar.current.isDate(rawSelectedDate, inSameDayAs: $0.date)
-        }
-    }
 
     var body: some View {
         NavigationStack {
@@ -41,73 +28,7 @@ struct DashboardView: View {
                     }
                     .pickerStyle(.segmented)
 
-                    VStack {
-                        NavigationLink(value: selectedStat) {
-                            HStack {
-                                VStack(alignment: .leading) {
-                                    Label("Steps", systemImage: "figure.walk")
-                                        .font(.title3.bold())
-                                        .foregroundStyle(.pink)
-
-                                    Text("Avg: \(avgStepCount) steps")
-                                        .font(.caption)
-                                        .foregroundStyle(.secondary)
-                                }
-
-                                Spacer()
-
-                                Image(systemName: "chevron.right")
-                                    .foregroundStyle(.secondary)
-                            }
-                        }
-                        .padding(.bottom, 12)
-
-                        Chart {
-                            if let selectedHealthMetric {
-                                RuleMark(x: .value("Selected Metric", selectedHealthMetric.date, unit: .day))
-                                    .foregroundStyle(Color.secondary.opacity(0.3))
-                                    .offset(y: -10)
-                                    .annotation(
-                                        position: .top,
-                                        spacing: 0,
-                                        overflowResolution: .init(x: .fit(to: .chart), y: .disabled)) {
-                                            annotationView
-                                        }
-                            }
-                            RuleMark(y: .value("Average", avgStepCount))
-                                .foregroundStyle(Color.secondary)
-                                .lineStyle(.init(lineWidth: 1, dash: [5, 10]))
-
-                            ForEach(hkManager.stepData) { steps in
-                                BarMark(
-                                    x: .value("Date", steps.date, unit: .day),
-                                    y: .value("Steps", steps.value)
-                                )
-                                .foregroundStyle(Color.pink.gradient)
-                                .opacity(rawSelectedDate == nil || steps.date == selectedHealthMetric?.date ? 1 : 0)
-                            }
-                        }
-                        .frame(height: 150)
-                        .chartXSelection(value: $rawSelectedDate.animation(.easeInOut))
-                        .onChange(of: rawSelectedDate, { oldValue, newValue in
-
-                        })
-                        .chartXAxis {
-                            AxisMarks {
-                                AxisValueLabel(format: .dateTime.month(.defaultDigits).day())
-                            }
-                        }
-                        .chartYAxis {
-                            AxisMarks { value in
-                                AxisGridLine()
-                                    .foregroundStyle(Color.secondary.opacity(0.3))
-
-                                AxisValueLabel((value.as(Double.self) ?? 0).formatted(.number.notation(.compactName)))
-                            }
-                        }
-                    }
-                    .padding()
-                    .background(RoundedRectangle(cornerRadius: 12).fill(Color(.secondarySystemBackground)))
+                    StepBarChart(selectedStat: selectedStat, chartData: hkManager.stepData)
 
                     VStack(alignment: .leading) {
                         VStack(alignment: .leading) {
@@ -148,24 +69,6 @@ struct DashboardView: View {
 
         }
         .tint(isSteps ? .pink : .indigo)
-    }
-
-    var annotationView: some View {
-        VStack(alignment: .leading) {
-            Text(selectedHealthMetric?.date ?? .now, format: .dateTime.weekday(.abbreviated).month(.abbreviated).day())
-                .font(.footnote.bold())
-                .foregroundStyle(.secondary)
-
-            Text(selectedHealthMetric?.value ?? 0, format: .number.precision(.fractionLength(0)))
-                .fontWeight(.heavy)
-                .foregroundStyle(.pink)
-        }
-        .padding(12)
-        .background(
-            RoundedRectangle(cornerRadius: 4)
-                .fill(Color(.secondarySystemBackground))
-                .shadow(color: .secondary.opacity(0.3), radius: 2, x: 2, y: 2)
-        )
     }
 }
 
