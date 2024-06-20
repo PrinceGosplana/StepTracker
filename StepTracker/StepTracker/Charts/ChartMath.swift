3654195ccb33acd75501d39810d656f366f4c370
Oleksandr Isaiev <nevkluchat@gmail.com>
2024-06-20 11:03:42 +0300

    add algorithm package

--- StepTracker/StepTracker/Charts/ChartMath.swift
+++ StepTracker/StepTracker/Charts/ChartMath.swift
@@ -5,12 +5,14 @@
 //  Created by Oleksandr Isaiev on 20.06.2024.
 //
 
+import Algorithms
 import Foundation
 
 struct ChartMath {
 
     static func averageWeekdayCount(for metric: [HealthMetric]) -> [WeekdayChartData] {
         let sortedByWeekday = metric.sorted { $0.date.weekdayInt < $1.date.weekdayInt }
+        let weekdayArray = sortedByWeekday
         return []
     }
 }
