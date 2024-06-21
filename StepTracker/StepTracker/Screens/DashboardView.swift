206c610887c28b0957ea769c15df9301afb0c468
Oleksandr Isaiev <nevkluchat@gmail.com>
2024-06-21 08:31:35 +0300

    piechart basic ui

--- StepTracker/StepTracker/Screens/DashboardView.swift
+++ StepTracker/StepTracker/Screens/DashboardView.swift
@@ -30,24 +30,7 @@ struct DashboardView: View {
 
                     StepBarChart(selectedStat: selectedStat, chartData: hkManager.stepData)
 
-                    VStack(alignment: .leading) {
-                        VStack(alignment: .leading) {
-                            Label("Averages", systemImage: "calendar")
-                                .font(.title3.bold())
-                                .foregroundStyle(.pink)
-
-                            Text("Last 28 Days")
-                                .font(.caption)
-                                .foregroundStyle(.secondary)
-                        }
-                        .padding(.bottom, 12)
-                        
-                        RoundedRectangle(cornerRadius: 12)
-                            .foregroundStyle(.secondary)
-                            .frame(height: 240 )
-                    }
-                    .padding()
-                    .background(RoundedRectangle(cornerRadius: 12).fill(Color(.secondarySystemBackground)))
+                    StepPieChart(chartData: ChartMath.averageWeekdayCount(for: hkManager.stepData))
                 }
             }
             .padding()
@@ -56,7 +39,6 @@ struct DashboardView: View {
             }
             .task {
                 await hkManager.fetchStepCount()
-                ChartMath.averageWeekdayCount(for: hkManager.stepData)
             }
             .navigationTitle("Dashboard")
             .navigationDestination(for: HealthMetricContext.self) { metric in
